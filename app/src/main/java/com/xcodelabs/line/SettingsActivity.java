c73841c8e17bfd745de1dc292134e4ac0b0a444c
jarvis coder <allinone3066@gmail.com>
2022-04-08 22:05:41 +0530

    change profile update code

--- app/src/main/java/com/xcodelabs/line/SettingsActivity.java
+++ app/src/main/java/com/xcodelabs/line/SettingsActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,11 +20,15 @@ import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
@@ -35,6 +41,7 @@ public class SettingsActivity extends AppCompatActivity {
     private StorageReference userProfileImageRef;
     private String downloadUrl;
     private DatabaseReference userRef;
+    private ProgressDialog progressDialog;
 
 
     @Override
@@ -49,6 +56,7 @@ public class SettingsActivity extends AppCompatActivity {
         userNameET = findViewById(R.id.username_settings);
         userBioET = findViewById(R.id.bio_settings);
         profileImageview = findViewById(R.id.settings_profile_image);
+        progressDialog = new ProgressDialog(this);
 
         profileImageview.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +74,7 @@ public class SettingsActivity extends AppCompatActivity {
                 saveUserData();
             }
         });
+        retrieveUserInfo();
     }
 
     @Override
@@ -82,11 +91,29 @@ public class SettingsActivity extends AppCompatActivity {
     private void saveUserData()
     {
         final String getUserName = userNameET.getText().toString();
-        final String getUserStatus = userNameET.getText().toString();
+        final String getUserStatus = userBioET.getText().toString();
 
         if (ImageUri == null)
         {
+            userRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                {
+                    if (dataSnapshot.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).hasChild("image"))
+                    {
+                        saveInfoOnlyWithoutImage();
+                    }else
+                    {
+                        Toast.makeText(SettingsActivity.this, "please select the image first", Toast.LENGTH_SHORT).show();
+                    }
+                }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error)
+                {
+
+                }
+            });
         }
         else if (getUserName.equals(""))
         {
@@ -98,6 +125,9 @@ public class SettingsActivity extends AppCompatActivity {
         }
         else
         {
+            progressDialog.setTitle("Account Settings");
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
             final StorageReference filepath = userProfileImageRef
                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
@@ -140,6 +170,7 @@ public class SettingsActivity extends AppCompatActivity {
                                     Intent intent = new Intent(SettingsActivity.this,ContactsActivity.class);
                                     startActivity(intent);
                                     finish();
+                                    progressDialog.dismiss();
 
                                     Toast.makeText(SettingsActivity.this, "profile information is updated...", Toast.LENGTH_SHORT).show();
                                 }
@@ -154,4 +185,75 @@ public class SettingsActivity extends AppCompatActivity {
             });
         }
     }
+
+    private void saveInfoOnlyWithoutImage()
+    {
+        final String getUserName = userNameET.getText().toString();
+        final String getUserStatus = userBioET.getText().toString();
+
+        if (getUserName.equals(""))
+        {
+            Toast.makeText(this, "userName is mandatory...", Toast.LENGTH_SHORT).show();
+        }
+        else if (getUserStatus.equals(""))
+        {
+            Toast.makeText(this, "bio is mandatory...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            progressDialog.setTitle("Account Settings");
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+
+            HashMap<String, Object>profileMap= new HashMap<>();
+            profileMap.put("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+            profileMap.put("name",getUserName);
+            profileMap.put("status",getUserStatus);
+            userRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .updateChildren(profileMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful())
+                    {
+                        Intent intent = new Intent(SettingsActivity.this,ContactsActivity.class);
+                        startActivity(intent);
+                        finish();
+                        progressDialog.dismiss();
+
+                        Toast.makeText(SettingsActivity.this, "profile information is updated...", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(SettingsActivity.this, "an Error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void retrieveUserInfo()
+    {
+        userRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists())
+                        {
+                            String imageDB = dataSnapshot.child("image").getValue().toString();
+                            String nameDB = dataSnapshot.child("name").getValue().toString();
+                            String bioDB = dataSnapshot.child("status").getValue().toString();
+
+                            userNameET.setText(nameDB);
+                            userBioET.setText(bioDB);
+                            Picasso.get().load(imageDB).placeholder(R.drawable.profile_image).into(profileImageview);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
 }
